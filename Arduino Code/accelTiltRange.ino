d2466da72fe25ddb58f8bd90593d079896fe0e40
T00FA4N <86071666+T00FA4N@users.noreply.github.com>
2022-05-26 09:09:12 -0400

    Update accelTiltRange.ino

--- Arduino Code/accelTiltRange.ino
+++ Arduino Code/accelTiltRange.ino
@@ -22,22 +22,28 @@ void loop() {
   
 
   if (az > 14000) {
-    Serial.println("Direction: Flat");  
+    Serial.println("Direction: Flat");
+    Serial.write("Flat", 5);
   }
   else if (ax >= 6000) {
     Serial.println("Direction: Left");
+    Serial.write("Left", 5);
   }
   else if (ax < -5000) {
-    Serial.println("Direction: Right");  
+    Serial.println("Direction: Right");
+    Serial.write("Rght", 5);
   }
   else if (ay < -5000) {
     Serial.println("Direction: Forward");  
+    Serial.write("Forw", 5);
   }
   else if (ay > 6400 || az > 13000) {
     Serial.println("Direction: Backward");  
+    Serial.write("Back", 5);
   }
   else {
     Serial.println("Direction: Unable to Detect");
+    Serial.write("Cont", 5);
     Serial.print("x: "); Serial.println(ax);
     Serial.print("y: "); Serial.println(ay);
     Serial.print("z: "); Serial.println(az);
