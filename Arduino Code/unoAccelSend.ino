66d6374496fea937966f2607bafc071254fadb98
T00FA4N <86071666+T00FA4N@users.noreply.github.com>
2022-06-02 09:10:33 -0400

    Change code for the sending information wirelessly between uno and nano.

--- Arduino Code/unoAccelSend.ino
+++ Arduino Code/unoAccelSend.ino
@@ -1,15 +1,23 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include <RH_ASK.h>
+#include <SPI.h>
 
 MPU6050 mpu;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
+RH_ASK d;
+char *msg = "";
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(38400);
-
   mpu.initialize();
+  
+  if (!d.init()) {
+    Serial.println("init failed");
+  }
 }
 
 void loop() {
@@ -24,19 +32,29 @@ void loop() {
 //S = stop, L = Left, R = Right
 //F = Forward, B = Back, C = Continue
   if (az > 14000) {
-    Serial.write("Stop\0");  
+    msg = "Stop\0";
+    d.send((uint8_t *)msg, strlen(msg));
+    d.waitPacketSent(); 
   }
   else if (ax >= 6000) {
-    Serial.write("Left\0");
+    msg = "Left\0";
+    d.send((uint8_t *)msg, strlen(msg));
+    d.waitPacketSent(); 
   }
   else if (ax < -5000) {
-    Serial.write("Rght\0");  
+    msg = "Rght\0";
+    d.send((uint8_t *)msg, strlen(msg));
+    d.waitPacketSent();  
   }
   else if (ay < -5000) {
-    Serial.write("Forw\0");  
+    msg = "Forw\0";
+    d.send((uint8_t *)msg, strlen(msg));
+    d.waitPacketSent(); 
   }
   else if (ay > 6400 || az > 13000) {
-    Serial.write("Back\0");  
+    msg = "Back\0";
+    d.send((uint8_t *)msg, strlen(msg));
+    d.waitPacketSent(); 
   }
 //  else {
 //    Serial.write("Cont\0");
