38ebc546d2958b481e8e4539b790ade3b0773fca
Sera <100382839+seraHamilton@users.noreply.github.com>
2022-06-13 11:34:35 -0400

    working graphs

--- Arduino Code/gui.py
+++ Arduino Code/gui.py
@@ -8,11 +8,9 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import pandas as pd
 
-import serialReader
-
-th = Thread(target=serialReader.init)
-th.start()
 sleep(1)
+
+        
 
 col_list = open("./data.csv", "r").readline().replace("\n", "").split(",")
 rcParams["toolbar"] = "None"
@@ -21,6 +19,7 @@ fig = plt.figure(figsize=(16, 9))
 fig.canvas.manager.set_window_title("Hand Accelerometer Controlled Car Live GUI")
 thismanager = plt.get_current_fig_manager()
 thismanager.window.wm_iconbitmap("") # Include filename of picture of icon on the GUI
+
 
 def animate(i):
     try:
@@ -31,19 +30,19 @@ def animate(i):
         direction = data["direction"]
         time = round(data["time"].tolist()[-1], 2)
         # This is the information you would get for speed, acceleration, and more. You can create a method above to solve it, if that's easier
-        speed = round(sqrt())
+        speed = data["speed"].astype(float)
         acceleration = 0 # Replace with calculated values
 
         gX, gY, gZ = (
             round(data["gX"].tolist()[-1], 2),
             round(data["gY"].tolist()[-1], 2),
-            round(data["gz"].tolist()[-1], 2),
+            round(data["gZ"].tolist()[-1], 2),
         )
 
         ax = plt.subplot(gs[0:1, 0])
         ax.set_title("Car Data")
-        ax.get_xaxis().setvisible(False)
-        ax.get_yaxis().setvisible(False)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
         text = f"""
 Status: {status}
@@ -74,27 +73,28 @@ Rotation: ({gX}, {gY}, {gZ})"""
         ax.legend(loc="upper left")
         
         ax = plt.subplot(gs[1, 0:2])
+        plt.plot(time_vals, speed, label="Velocity", color="black")
+        ax.set_title("Speed")
+        ax.legend(loc="upper left")
+
+        ax = plt.subplot(gs[1, 2])
         plt.plot(time_vals, data["aX"].astype(float), label="x", color="red")
         plt.plot(time_vals, data["aY"].astype(float), label="y", color="blue")
         plt.plot(time_vals, data["aZ"].astype(float), label="z", color="green")
         ax.set_title("Accelerometer")
-        ax.legend(loc="upperleft")
-
-        ax = plt.subplot(gs[1, 2])
-        plt.plot(time_vals, speed, label="Velocity", color="black")
-        ax.set_title("Speed")
-        ax.legend(loc="upperleft")
+        ax.legend(loc="upper left")
 
         ax = plt.subplot(gs[2, 0:2])
         plt.plot(time_vals, speed, label="Acceleration", color="black")
         ax.set_title("Acceleration")
-        ax.legend(loc="upperleft")
+        ax.legend(loc="upper left")
 
-        ax=plt.subplot(gs[2, 2])
+        """ax=plt.subplot(gs[2, 2])
         plt.plot(time_vals, fdirec, label="pos f", color="red")
         plt.plot(time_vals, bdirec, label="pos b", color ="blue")
         plt.plot(time_vals, ldirec, label="pos l", color ="green")
         plt.plot(time_vals, rdirec, label="pos r", color="yellow")
+        """
 
     except Exception as e:
         print(e)
